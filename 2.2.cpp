14fc22f7d41e25118d47545c8d99b7fed8392b22
Олеся <olesyob20060720@gmail.com>
2024-11-27 21:36:01 +0500

    Add files via upload

---
+++ 2.2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n; int product = 1;
+	cin >> n;
+	if (n <= 0)
+	{
+		cout << "Ќельз€ посчитать произведение чисел при n<=0" << endl;
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			product *= i;
+		}
+		cout << product << endl;
+	}
+}
