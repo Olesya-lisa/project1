f3c72a49b16a70c1e27e9eb97cce48067e7e3ed4
Олеся <olesyob20060720@gmail.com>
2024-12-05 01:51:29 +0500

    Add files via upload

---
+++ 2.7.1.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,k;
+	cin >> n;
+	k = sqrt(n);
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
