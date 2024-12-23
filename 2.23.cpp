f4d57c65b40d38230732eab805f7618a83b82b6f
Олеся <olesyob20060720@gmail.com>
2024-12-23 21:19:52 +0500

    Add files via upload

---
+++ 2.23.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	float n, a, max;
+	int i=2;
+	int count= 1;
+	cin >> n;
+	max = sin(n + 1 / n);
+	while (i <= n)
+	{
+		a = sin(n + i / n);
+		if (a > max)
+		{
+			max = a;
+			count= i;
+		}
+		else if (a == max)
+		{
+			count++;
+		}
+		i++;
+	}
+	cout << "Максимальный элемент:" <<max<< endl;
+	cout << "Число элементов с этими значением:" << count<< endl;
+}
+
